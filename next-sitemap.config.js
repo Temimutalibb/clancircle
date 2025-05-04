fc7d05129ff34ee60d1e3fe62e5dd269e5f73844
Mutalibb <temitopking@icloud.com>
2025-05-04 02:19:48 +0100

    optimized for google seaarch

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://clancircle.mutalibb.xyz",
+  generateRobotsTxt: true,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Clan Circle",
-  description: "Click to earn",
+  title: "Clan Circle - Boost Content with $CLAN",
+  description:
+    "Clan Circle helps users grow engagement through social sharing, check-ins, and rewards using $CLAN.",
+  keywords: [
+    "Clan Circle",
+    "$CLAN",
+    "check-in",
+    "social boost",
+    "engagement tool",
+  ],
+  metadataBase: new URL("https://clancircle.mutalibb.xyz"),
+  openGraph: {
+    title: "Clan Circle",
+    description:
+      "Clan Circle is a content engagement platform where users earn $CLAN coins by sharing and clicking social links. Boost your visibility, join circles, and stay notified with push alerts and check-ins.",
+    url: "https://clancircle.mutalibb.xyz",
+    siteName: "Clan Circle",
+    images: [
+      {
+        url: "/new506.png", // make sure this image exists in /public
+        width: 1200,
+        height: 630,
+        alt: "Clan Circle OG Image",
+      },
+    ],
+    type: "website",
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
